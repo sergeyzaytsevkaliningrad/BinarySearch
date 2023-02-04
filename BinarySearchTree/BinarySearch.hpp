fcf5d955794a84056ccf61d84f058b4dd03aed31
ZA_iOS <sergey.zaytsev.kaliningrad@gmail.com>
2023-02-04 16:33:56 +0300

    Initial Commit

---
+++ BinarySearchTree/BinarySearch.hpp
@@ -0,0 +1,13 @@
+//
+//  BinarySearch.hpp
+//  BinarySearchTree
+//
+//  Created by Сергей Зайцев on 01.02.2023.
+//
+
+#ifndef BinarySearch_hpp
+#define BinarySearch_hpp
+
+#include <stdio.h>
+
+#endif /* BinarySearch_hpp */
---
+++ BinarySearchTree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BinarySearchTree
+//
+//  Created by Сергей Зайцев on 01.02.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
